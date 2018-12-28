963f3b7c84cd912d43a5cee55e30544a456699a9
plutasnyy <kamil.plucinski97@gmail.com>
2018-12-28 15:45:24 +0100

    created endpoint

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.db import connection
+from django.http import JsonResponse
 
-# Create your views here.
+from core.models import BloodType
+
+
+def resources(request):
+    response_data = dict()
+    with connection.cursor() as cursor:
+        cursor.callproc('getavailableblood')
+        result = cursor.fetchall()
+        for (quantity, id) in result:
+            blood = BloodType.objects.get(pk=id)
+            blood_name = '{} Rh{}'.format(blood.blood, blood.rh)
+            response_data[id] = {
+                'quantity': quantity,
+                'blood_name': blood_name
+            }
+    return JsonResponse(response_data)
